ded7be8ea9d23ee70ab264e3ef39150763726e51
izwrt <ishwart466@gmail.com>
2024-06-20 07:25:44 +0530

    header is added

--- App.js
+++ App.js
@@ -35,20 +35,38 @@ function Header(){
     );
   };
 
+  const styleCard = {
+    
+  };
+
+  const RestaurantCard = () => {
+    return(
+      <div className="res-card">
+        <img className="res-card-img" src="https://cloudfront.frive.co.uk/media/5452/opt_upload.png"/>
+        <h3>Blueberry Pancakes</h3>
+        <h4>South Indian, Beverages Malad Kan East</h4>
+        <h4>38 min <span>4.8 star</span></h4>
+      </div>
+    )
+  }
+
   const Body = () => {
-    <div className="body">
+    return(
+      <div className="body">
         <div className="search">Serach</div>
-        <div className="res-container">
-            
-        </div>
-    </div>
-}
+          <div className="res-container">
+            <RestaurantCard/>
+          </div>
+      </div>
+    )
+  }
 
   
 const AppLoyout = () => {
     return(
         <div className="app">
             <Header />
+            <Body/>
         </div>
     )
 }
