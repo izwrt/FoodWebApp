12dfffd222c942d91bb45cf43376488eaa09d493
izwrt <ishwart466@gmail.com>
2024-06-23 19:57:15 +0530

    added shimmer UI

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,23 +1,50 @@
 import RestaurantCard from "./RestaurantCard";
-import infolist from "../utils/mockData";
-import { useState } from 'react';
+import Shimmer from "./Shimmer";
+import { FOOD_API } from "../utils/contant";
+import { useState, useEffect } from 'react';
 
 const Body = () => {
-const [cardList, setCardList] = useState(infolist);
+const [cardList, setCardList] = useState([]);
 
-console.log(cardList);
-    return(
+useEffect(()=>{
+  fetchData()
+},[]);
+
+const fetchData = async () =>{
+  const realData = await fetch(FOOD_API);
+  const json = await realData.json();
+  setCardList(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  console.log(json);
+};
+
+
+
+// console.log(cardList);
+    return cardList.length === 0 ? (
       <div className="body">
-        <button className="filter-btn" onClick={() =>{
-          setCardList(cardList.filter(card => card.info.avgRating > 4.7))
+          <button className="shimmer-filter-btn" ></button>
+          <div className="shimmer-res-container">
+            {[...Array(10)].map((_,i) => 
+                <Shimmer key={i}/>
+            )}
+          </div>
+    </div>
+    ):(
+      <div className="body">
+        <button className="filter-btn" 
+        
+        onClick={() =>{
+          setCardList(cardList.filter(card => card.info.avgRating > 4.4))
         }}>Top Rated Restaurant</button>
-          <div className="res-container">
 
+        
+          <div className="res-container">
+          
           {cardList.map((data, index) => 
           <RestaurantCard key={data.info.id} data={data.info} />
           
           )
-          };
+          }
           
           </div>
       </div>
---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,14 @@
+const Shimmer = () => {
+    return (
+        <div className="shimmer-res-card">
+        <div className="shimmer-card-img-container">
+        <img className="res-card-img" />
+        </div>
+        <h3></h3>
+        <h5></h5>
+        <h4><span></span></h4>
+      </div>
+    )
+}
+
+export default Shimmer;
