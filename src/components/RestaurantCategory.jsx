66fdd70556679deea894328233a0bcc8da7e8054
izwrt <ishwart466@gmail.com>
2024-08-12 12:17:52 +0530

    itro to redux

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -15,10 +15,10 @@ const handleClick = (e) =>{
    
 }
     return (
-        <div className="w-6/12 m-auto bg-gray-100 my-5 cursor-pointer  rounded-sm shadow-sm" onClick={handleClick}>
-            <div className="flex justify-between p-2 px-4">
+        <div className="w-6/12 m-auto bg-gray-100 my-5 rounded-sm shadow-sm" >
+            <div className="flex justify-between p-2 px-4 cursor-pointer" onClick={handleClick}>
             <span className="font-extrabold">{data?.title} ({data?.itemCards.length})</span>
-            <span>🔽</span>
+            <span></span>
             </div>
             <div>
                 {showItems && <ItemList data={data.itemCards}/> }
